f9af21dc6ed22e883a8b356c0a907ea22af6e478
gyl <22400056@qq.com>
2018-08-05 21:52:18 +0800

    第一篇: 服务的注册与发现（Eureka）(Finchley版本)

---
+++ ss/src/main/java/cn/aa/ss/SsApplication.java
@@ -0,0 +1,14 @@
+package cn.aa.ss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsApplication.class, args);
+	}
+}
